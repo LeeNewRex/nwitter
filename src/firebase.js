2e71485d27887085631129bd60c8ecebb2d43f73
JabdaDev <92113070+JabdaDev@users.noreply.github.com>
2021-12-30 00:47:07 +0900

    firebase add

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase, { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: "1:323700813454:web:6475cb59d40bcf73bba3df"
+  };
+
+ const app = initializeApp(firebaseConfig);
+
+ export default app;
